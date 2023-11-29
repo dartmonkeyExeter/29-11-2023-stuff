96cd9a8207075a90732b2c07f63123ab4d697f59
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-29 14:22:04 +0000

    HEHEHEHA

--- # madlibs.py
+++ # madlibs.py
@@ -1,20 +1,16 @@
 # madlibs.py
 from random import randint
-
-stories = ["The {0} {1} {2} {3} the {4} {5}.",]
-types = [["adjective","adjective","animal","verb","adjective","animal"]]
-
+stories = ["The {0} {1} {2} {3} the {4} {5}.",
+            "How much {0} could a {1} {2}, if a {1} could {2} {0}?",
+            "{0} sells {1} on the {2}, but the value of those {1} will fall, due to the laws of supply and demand, no one wants to buy {1} when theres loads on the {3}!"]
+types = [["adjective","adjective","animal","verb","adjective","animal"], ["noun", "animal", "verb"], ["pronoun","noun","place","place"]]
 def gameloop():
-    which = randint(0,len(stories))
+    which = randint(0,len(stories) - 1)
     story = stories[which]
-    cur_type = ["adjective","adjective","animal","verb","adjective","animal"]
+    cur_type = types[which]
     words = []
-
-    for i in range(len(types)):
-        words.append(input(f"{types[i]}: "))
-
+    for i in range(len(cur_type)):
+        words.append(input(f"{cur_type[i]}: "))
     mad_lib = story.format(*words)
-
     print(mad_lib)
-
 gameloop()
